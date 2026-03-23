74ba1b7eac3163ae43085d1444fa4f48eeb5cc7e
ippur <ti@ippur.pa.gov.br>
2026-03-23 13:51:03 -0300

    fix: ajustar transporter smtp para usar variaveis do env (porta 587)

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -5,8 +5,8 @@ dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT || 465),
-  secure: String(process.env.EMAIL_SECURE) === "true",
+  port: Number(process.env.EMAIL_PORT),
+  secure: process.env.EMAIL_SECURE === "true",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
