296fd2e3af91c9f50317ce5c5f3df5e8a10643b3
ippur <ti@ippur.pa.gov.br>
2026-03-23 13:41:39 -0300

    fix: priorizar ipv4 no envio smtp para formulario de contato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,4 +1,7 @@
+import dns from "dns";
 import nodemailer from "nodemailer";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
