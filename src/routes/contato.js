162342169846864bddd32c3e57ba1212ec90b1a9
ippur <ti@ippur.pa.gov.br>
2026-03-24 09:14:22 -0300

    debug: confirmar carregamento da rota de contato no backend

--- src/routes/contato.js
+++ src/routes/contato.js
@@ -2,6 +2,8 @@ import express from "express";
 import multer from "multer";
 import { enviarContato } from "../controllers/contatoController.js";
 
+console.log("=== ROTA CONTATO ATIVA ===");
+
 const router = express.Router();
 
 const uploadContato = multer({
